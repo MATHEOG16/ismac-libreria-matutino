7cc44cfc62023806a9ded220dbaa204df5353f78
1755430293 <mgalvez@tecnologicoismac.edu.ec>
2025-08-22 09:19:58 -0500

    Srping Boot

--- libreria-spring/src/main/java/com/distribuida/config/WebConfig.java
+++ libreria-spring/src/main/java/com/distribuida/config/WebConfig.java
@@ -9,18 +9,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer(){
-        return new WebMvcConfigurer(){
-            public void addCorsMappings(CorsRegistry corsRegistry) {
-                corsRegistry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        System.out.println("🚀 Configuración CORS cargada correctamente");
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
+
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
